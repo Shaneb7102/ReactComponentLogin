e02f3e9146ade0cb917bff14b40a4b8c8c01e43c
Shane Barden <shanebar@live.ie>
2025-02-25 01:00:20 +0000

    dsf

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <p>Hello World</p>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <p>Hello World</p>
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
