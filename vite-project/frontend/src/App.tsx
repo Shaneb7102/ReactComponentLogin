f2c2f938dd20cab558f217d2f19541d9a7959746
Shane Barden <shanebar@live.ie>
2025-03-05 14:11:34 +0000

    Switching BrowserRouter to HashRouter

--- vite-project/frontend/src/App.tsx
+++ vite-project/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -9,28 +9,29 @@ import AuthGuard from "./components/AuthGuard";
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route 
-  path="/admin" 
-  element={
-    <AuthGuard>
-      <AdminPage />
-    </AuthGuard>
-  } 
-/>
-      {/* Protected route that requires authentication */}
-      <Route 
-        path="/end" 
-        element={
-          <AuthGuard>
-            <EndPage />
-          </AuthGuard>
-        } 
-      />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route 
+          path="/admin" 
+          element={
+            <AuthGuard>
+              <AdminPage />
+            </AuthGuard>
+          } 
+        />
+        <Route 
+          path="/end" 
+          element={
+            <AuthGuard>
+              <EndPage />
+            </AuthGuard>
+          } 
+        />
+      </Routes>
+    </HashRouter>
   );
 };
 
